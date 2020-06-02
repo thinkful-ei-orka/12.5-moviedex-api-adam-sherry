ae7dd7d67a162697b9d04c4f297284fff1ea2680
Adam Brown <adam.edward.brown@gmail.com>
2020-06-02 15:19:38 -0400

    completed assignemnt requirements

--- app.js
+++ app.js
@@ -8,25 +8,33 @@ const app = express();
 app.use(cors());
 app.use(helmet());
 app.use(morgan('combined'));
-app.use(handleBearerToken);
 
 const apiToken = process.env.API_TOKEN;
 
 function handleBearerToken(req,res,next){
-  const authToken = req.get('Authorization')||'';
+  const authToken = req.get('Authorization')||' ';
   if(!authToken.toLowerCase().startsWith('bearer ')){
-    res.status(400).json({error:'No valide bearer token provided'});
+    return res.status(400).json({error:'No valid bearer token provided'});
   }
   if(authToken.split(' ')[1]!==apiToken){
-    res.status(401).json({error:'Invalid credentials'});
+    return res.status(401).json({error:'Invalid credentials'});
   }
   next();
 }
 
+app.use(handleBearerToken);
+
 const handleResponse = (req,res)=>{
-  res.json(Response);
+  let result = data;
+  const filtering=(arr,value)=>arr.filter(x=>x[value].toLowerCase().includes(req.query[value].toLowerCase()));
+  
+  if(req.query.genre){result=filtering(result,'genre');}
+  if(req.query.country){result=filtering(result,'country');}
+  if(req.query.avg_vote){result=result.filter(movie=>movie.avg_vote>=req.query.avg_vote);}
+
+  res.json(result);
 };
 
-app.get('/route',handleResponse);
+app.get('/movie',handleResponse);
 
 app.listen(8080,()=>console.log('Running on 8080'));
